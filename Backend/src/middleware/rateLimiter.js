459aa4577f5ba753b4df2b9c2910d3b1697ee783
Nguyen Tang Minh Thong <thongntmse171742@fpt.edu.vn>
2025-07-18 10:01:47 +0700

    fix limit login to fail count =10, block sign 2minutes

--- Backend/src/middleware/rateLimiter.js
+++ Backend/src/middleware/rateLimiter.js
@@ -14,8 +14,8 @@ const rateLimiter = rateLimit({
 });
 
 const strictRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 5 requests per windowMs
+  windowMs: 2 * 60 * 1000, // 2 minutes
+  max: 10, // limit each IP to 50 requests per windowMs
   message: {
     success: false,
     message: 'Too many login attempts from this IP, please try again later.'
